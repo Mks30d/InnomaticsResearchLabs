daf0ed1833af1e9113f335dbc5a94c5fd54dc482
MANISH KUMAR SHARMA <112253925+Mks30d@users.noreply.github.com>
2023-03-30 23:29:40 +0530

    Add files via upload

---
+++ 6Assignment Ecommerce Layout/src/App.js
@@ -0,0 +1,18 @@
+import Ecommerce from './Components/Ecommerce';
+import Navbar from './Components/Navbar';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div>
+      <div>
+      <Navbar />
+      <Ecommerce />
+      </div>
+    </div>
+  );
+}
+
+export default App;
